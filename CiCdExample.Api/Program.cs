747db6f58b3465e94157ee9845dca0ca600db508
TomkaYurii <tomka.yurii@gmail.com>
2022-05-25 02:18:13 +0300

    fianl

--- CiCdExample.Api/Program.cs
+++ CiCdExample.Api/Program.cs
@@ -1,6 +1,11 @@
+using CiCdExample.Api;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<CustomerContext>(x => x.UseSqlServer(connectionString));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
--- CiCdExample.Tests/UnitTest1.cs
+++ CiCdExample.Tests/UnitTest1.cs
@@ -1,3 +1,8 @@
+using CiCdExample.Api;
+using CiCdExample.Api.Controllers;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
 namespace CiCdExample.Tests
 {
     public class UnitTest1
@@ -6,6 +11,36 @@ namespace CiCdExample.Tests
         public void Test1()
         {
             Assert.True(1 == 1);
+        }
+
+        [Fact]
+        public async Task CustomerIntegrationTest()
+        {
+            // Create DB Context
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .AddEnvironmentVariables()
+                .Build();
+
+            var optionsBuilder = new DbContextOptionsBuilder<CustomerContext>();
+            optionsBuilder.UseSqlServer(configuration["ConnectionStrings:DefaultConnection"]);
+
+            var context = new CustomerContext(optionsBuilder.Options);
+
+            // Just to make sure: Delete all existing customers in the DB
+            await context.Database.EnsureDeletedAsync();
+            await context.Database.EnsureCreatedAsync();
+
+            // Create Controller
+            var controller = new CustomersController(context);
+
+            // Add customer
+            await controller.Add(new Customer() { CustomerName = "FooBar" });
+
+            // Check: Does GetAll return the added customer?
+            var result = (await controller.GetAll()).ToArray();
+            Assert.Single(result);
+            Assert.Equal("FooBar", result[0].CustomerName);
         }
     }
 }
